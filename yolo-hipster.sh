8165a9a3ef45b1ef6fe5e91166d39d7568a1b311
treckstar <treckstar@gmail.com>
2014-02-22 23:40:15 -0500

    Test script

--- yolo-hipster.sh
+++ yolo-hipster.sh
@@ -5,17 +5,14 @@
 NOW=$(date +"%F")
 NOWT=$(date +"%T")
 
-GIT_USER="/home/brandon/git-username"
-GIT_PASS="/home/brandon/git-password"
-
 STUFF=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c2048);
 
-echo $STUFF > "/home/brandon/yolo-octo-hipster/hipster-files/$NOW-$NOWT"
+echo $STUFF > "/root/yolo-octo-hipster/hipster-files/$NOW-$NOWT"
 
 git pull origin master
 
 git add -A
 
-git commit -a -m "Test"
+git commit -a -m "Test script"
 
 git push origin master
