a7243336ae375c3f2bd231e820038e30a3ebe443
trecki <brandon.trecki@zapsolutions.com>
2014-02-22 23:23:11 -0500

    Test

---
+++ yolo-hipster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## date format ##
+NOW=$(date +"%F")
+NOWT=$(date +"%T")
+
+STUFF=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c2048);
+
+echo $STUFF > "/home/brandon/yolo-octo-hipster/hipster-files/$NOW-$NOWT"
+
+git add -A
+
+git commit -a -m "Test"
+
+get push origin master
