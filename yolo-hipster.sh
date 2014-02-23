87803b375e5f9d95d857d0f781641f6176797f30
trecki <brandon.trecki@zapsolutions.com>
2014-02-22 23:28:03 -0500

    Test

--- yolo-hipster.sh
+++ yolo-hipster.sh
@@ -4,12 +4,24 @@
 NOW=$(date +"%F")
 NOWT=$(date +"%T")
 
+GIT_USER="/home/brandon/git-username"
+GIT_PASS="/home/brandon/git-password"
+
 STUFF=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c2048);
 
 echo $STUFF > "/home/brandon/yolo-octo-hipster/hipster-files/$NOW-$NOWT"
+
+git pull origin master
 
 git add -A
 
 git commit -a -m "Test"
 
 git push origin master
+
+expect "Username:"
+# Send the username, and then wait for a password prompt.
+send "$GIT_USER\r"
+expect "Password:"
+# Send the password, and then wait for a shell prompt.
+send "$GIT_PASS\r"
